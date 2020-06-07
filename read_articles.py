5caafe3bfe9740e1e1f6d98bfb6c00b4b1d80a53
zl9901 <46223720+zl9901@users.noreply.github.com>
2020-06-06 22:10:56 -0400

    Add files via upload

---
+++ read_articles.py
@@ -0,0 +1,74 @@
+
+
+
+import json as js
+import os
+
+def read_articles():
+    i = 0
+    """
+    walk through all the files under specific directory
+    """
+    for dirname, _, filenames in os.walk('C:/PythonWorkspace/document_parses/pdf_json'):
+        for filename in filenames:
+            #print(os.path.join(dirname, filename))
+
+
+            if i % 1000 == 0:
+                print ("Working (number %d)..." % i)
+
+            """
+            only load json files
+            """
+            if filename.split(".")[-1] == "json":
+
+                f = open(os.path.join(dirname, filename))
+                j = js.load(f)
+                f.close()
+
+                """
+                some articles contain abstract while others not 
+                """
+                try:
+                    abstract_text = ' '.join([x['text'] for x in j['abstract']])
+                except:
+                    abstract_text = ''
+
+
+                cnt=0
+                name = ''
+                # d represents each individual dictionary
+                # j['body_text'] is a python list which contains many dictionaries
+                for d in j['body_text']:
+                    # preprocess the data which will bu put in csv file
+                    if cnt<1:
+                        for dic in j['metadata']['authors']:
+                            name += dic['first'] + ' ' + dic['last']
+                            name += ', '
+                        name = name[:-2]
+                        cnt+=1
+
+
+
+                """
+                body text and abstract consist the whole body text
+                """
+                body_text = ' '.join(x['text'] for x in j['body_text'])
+                body_text += ' ' + abstract_text
+
+                # use i to print
+                if i==199:
+                    print('authors are ' + name)
+                    if abstract_text:
+                        print(abstract_text)
+                    else:
+                        print(body_text)
+                # use i to read specific article
+                    exit()
+                i+=1
+    return
+
+
+read_articles()
+
+
