4b73477877074018e5aa9c37b4b6e6c3be00325c
zl9901 <46223720+zl9901@users.noreply.github.com>
2020-06-13 17:25:40 -0400

    Update CSCI788 Capstone.py

--- CSCI788 Capstone.py
+++ CSCI788 Capstone.py
@@ -29,7 +29,13 @@ import xlsxwriter
 import random
 import re
 import xlrd
-from sklearn.svm import SVR
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import *
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold
+
+import collections
 
 import tensorflow as tf
 import tensorflow_hub as hub
@@ -101,8 +107,8 @@ def preprocess_data():
             """
             This is for test purpose
             """
-            # if i==1000:
-            #     return corona_body_all_text, corona_abstract_all_text, spreadsheet_match, keywords_list
+            if i==1000:
+                return corona_body_all_text, corona_abstract_all_text, spreadsheet_match, keywords_list
 
             """
             only load json files
@@ -205,9 +211,12 @@ def word_cloud_advanced(corona_pos_all_text):
     plt.show()
 
 
-
+"""
+this function is to generate the histogram
+"""
 def generate_histogram(corona_body_all_text):
     # corona_hist = [len(preprocess_stem_clean(x)) for x in corona_body_all_text]
+    # remove all the punctuations at the very beginning
     corona_hist = [len(re.findall(r'\w+', x))  for x in corona_body_all_text]
     plt.hist(corona_hist,bins='auto',color='#0504aa',alpha=0.7)
     # plt.bar([i for i in range(len(corona_pos_all_text))], corona_pos_all_text)
@@ -260,10 +269,13 @@ def generate_tfidf(corona_body_all_text):
     for text in corona_body_all_text:
         corpus.append(' '.join(text))
 
+    # the features of each data are 900 now
     vectorizer=TfidfVectorizer(max_features=900)
     corpus_matrix=vectorizer.fit_transform(corpus)
     print(corpus_matrix.shape)
-    # the most representative top 5000 words in corona_body_all_text
+
+    # the most representative top 900 words in corona_body_all_text
+    # the basic structure is like a python dictionary
     word_feature_list=vectorizer.get_feature_names()
     corpus_matrix=corpus_matrix.toarray()
     return corpus_matrix,word_feature_list
@@ -286,29 +298,230 @@ def generate_labels():
         indices.append(int(sheet.cell_value(i,3)))
     return y,indices
 
+
 """
-go through svm iteration
+grid search function
 """
-def generate_SVM(corpus_matrix,y,indices,spreadsheet_match):
-    svr_rbf=SVR(kernel='rbf',gamma='scale')
-    X=[]
+def grid_search(corpus_matrix,y,indices,spreadsheet_match):
+    # this is for unbalanced problems
+    cw=collections.Counter(y)
+
+    # examples of grid search
+    parameters = {'kernel': ['linear', 'poly', 'rbf'], 'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 'C': [0.1,1,10,100,1000], 'class_weight':[cw]}
+    grid = GridSearchCV(SVC(), parameters, refit=True, verbose=3)
+
+    X = []
     for i in range(len(corpus_matrix)):
         if i in indices:
             # train on 200 papers
             X.append(corpus_matrix[i])
-    # predict on the entire dataset
-    y_score_rbf=svr_rbf.fit(X,y).predict(corpus_matrix)
 
+    clf = grid.fit(X, y)
+
+    # print(clf.best_params_)
+    # print(clf.best_estimator_)
+    # print()
+
+    # predict on the entire dataset
+    y_score_rbf = clf.decision_function(corpus_matrix)
+
+    # this yields the minimum and maximum value of svm regressor output
+    print(str(min(y_score_rbf)) + '   ' + str(max(y_score_rbf)))
+    print()
+
+    # return index of a sorted list
+    sorted_indices = sorted(range(len(y_score_rbf)), key=lambda k: y_score_rbf[k])
+
+    # get the paper numbers which are not in the training set
+    subtract_indices = []
+    for val in sorted_indices:
+        if val not in indices:
+            subtract_indices.append(val)
+
+    # get top 10%, bottom 10% and middle 10%
+    res = []
+    res += subtract_indices[:67]
+    res += subtract_indices[-67:]
+    res += subtract_indices[len(sorted_indices) // 2:len(sorted_indices) // 2 + 33]
+    res += subtract_indices[len(sorted_indices) // 2 - 33:len(sorted_indices) // 2]
+
+    workbook = xlsxwriter.Workbook('spreadsheet_svm.xlsx')
+    worksheet = workbook.add_worksheet('My sheet')
+
+    # write to a file without labels, this is different from the initial file
+    # each paper is labeled 1 automatically at the very beginning
+    # then we need to label each paper manually
+    header = ['paper_id', 'title', 'authors', 'paper_number', 'label']
+    for k in range(len(header)):
+        worksheet.write(0, k, header[k])
+
+    for i in range(1, len(res) + 1):
+        for j in range(len(spreadsheet_match[0])):
+            worksheet.write(i, j, spreadsheet_match[res[i - 1]][j])
+    workbook.close()
+
+
+"""
+k-fold analysis function
+"""
+def k_fold_svm(corpus_matrix,y,indices,spreadsheet_match):
+    # this is for unbalanced problems
+    cw = collections.Counter(y)
+
+    # initialize an SVM rbf kernel
+    svc_rbf = SVC(kernel='rbf', gamma='scale', class_weight=cw)
+    X = []
+
+    for i in range(len(corpus_matrix)):
+        if i in indices:
+            # train on 200 papers
+            X.append(corpus_matrix[i])
+
+    X=np.array(X)
+    y=np.array(y)
+
+    scores=[]
+    cv=KFold(n_splits=10,random_state=32,shuffle=True)
+    store_X=[]
+    store_y=[]
+    for train_index,test_index in cv.split(X):
+        X_train,X_test,y_train,y_test=X[train_index],X[test_index],y[train_index],y[test_index]
+        svc_rbf.fit(X_train,y_train)
+        scores.append(svc_rbf.score(X_test,y_test))
+        store_X.append(X_train)
+        store_y.append(y_train)
+
+    pos=scores.index(max(scores))
+    X=store_X[pos].tolist()
+    y=store_y[pos].tolist()
+
+
+    # predict on the entire dataset
+    y_score_rbf = svc_rbf.fit(X, y).decision_function(corpus_matrix)
+
+    # this yields the minimum and maximum value of svm regressor output
+    print(str(min(y_score_rbf)) + '   ' + str(max(y_score_rbf)))
+    print()
+
+    def normalization(data):
+        _range = np.max(data) - np.min(data)
+        return (data - np.min(data)) / _range
+
+    # this is for f1_score, recall, accuracy and ROC curve
+    training_metrics = svc_rbf.fit(X, y)
+    training_score = training_metrics.decision_function(X)
+    training_score_normalization = normalization(training_score)
+    training_label = training_metrics.predict(X)
+    print('accuracy ' + str(accuracy_score(y, training_label)))
+    print('f1_score ' + str(f1_score(y, training_label)))
+    print('recall_score ' + str(recall_score(y, training_label)))
+
+    fpr, tpr, threshold = roc_curve(y, training_score_normalization)
+
+    # plot the ROC curve
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2)
+    plt.xlim([0.0, 1.05])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('The ROC curve')
+    plt.show()
+
+    # return index of a sorted list
+    sorted_indices = sorted(range(len(y_score_rbf)), key=lambda k: y_score_rbf[k])
+
+    # get the paper numbers which are not in the training set
+    subtract_indices = []
+    for val in sorted_indices:
+        if val not in indices:
+            subtract_indices.append(val)
+
+    # get top 10%, bottom 10% and middle 10%
+    res = []
+    res += subtract_indices[:67]
+    res += subtract_indices[-67:]
+    res += subtract_indices[len(sorted_indices) // 2:len(sorted_indices) // 2 + 33]
+    res += subtract_indices[len(sorted_indices) // 2 - 33:len(sorted_indices) // 2]
+
+    workbook = xlsxwriter.Workbook('spreadsheet_svm.xlsx')
+    worksheet = workbook.add_worksheet('My sheet')
+
+    # write to a file without labels, this is different from the initial file
+    # each paper is labeled 1 automatically at the very beginning
+    # then we need to label each paper manually
+    header = ['paper_id', 'title', 'authors', 'paper_number', 'label']
+    for k in range(len(header)):
+        worksheet.write(0, k, header[k])
+
+    for i in range(1, len(res) + 1):
+        for j in range(len(spreadsheet_match[0])):
+            worksheet.write(i, j, spreadsheet_match[res[i - 1]][j])
+    workbook.close()
+
+
+
+
+
+"""
+go through svm iteration
+"""
+def generate_SVM(corpus_matrix,y,indices,spreadsheet_match):
+
+    # this is for unbalanced problems
+    cw=collections.Counter(y)
+
+    # initialize an SVM rbf kernel
+    svc_rbf=SVC(kernel='rbf',gamma='scale',class_weight=cw)
+    X=[]
+
+    for i in range(len(corpus_matrix)):
+        if i in indices:
+            # train on 200 papers
+            X.append(corpus_matrix[i])
+
+    # predict on the entire dataset
+    y_score_rbf=svc_rbf.fit(X,y).decision_function(corpus_matrix)
+
+    # this yields the minimum and maximum value of svm regressor output
     print(str(min(y_score_rbf))+'   '+str(max(y_score_rbf)))
+    print()
+
+    def normalization(data):
+        _range = np.max(data) - np.min(data)
+        return (data - np.min(data)) / _range
+
+    # this is for f1_score, recall, accuracy and ROC curve
+    training_metrics=svc_rbf.fit(X,y)
+    training_score=training_metrics.decision_function(X)
+    training_score_normalization = normalization(training_score)
+    training_label=training_metrics.predict(X)
+    print('accuracy '+str(accuracy_score(y,training_label)))
+    print('f1_score '+str(f1_score(y,training_label)))
+    print('recall_score '+str(recall_score(y,training_label)))
+
+    fpr,tpr,threshold=roc_curve(y,training_score_normalization)
+
+    # plot the ROC curve
+    plt.figure()
+    plt.plot(fpr,tpr, color='darkorange', lw=2)
+    plt.xlim([0.0, 1.05])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('The ROC curve')
+    plt.show()
 
     # return index of a sorted list
     sorted_indices=sorted(range(len(y_score_rbf)),key=lambda k:y_score_rbf[k])
 
 
+    # get the paper numbers which are not in the training set
     subtract_indices=[]
     for val in sorted_indices:
         if val not in indices:
             subtract_indices.append(val)
+
 
     # get top 10%, bottom 10% and middle 10%
     res=[]
@@ -321,7 +534,10 @@ def generate_SVM(corpus_matrix,y,indices,spreadsheet_match):
     workbook=xlsxwriter.Workbook('spreadsheet_svm.xlsx')
     worksheet=workbook.add_worksheet('My sheet')
 
-    # rewrite a file, this is different from the initial one
+
+    # write to a file without labels, this is different from the initial file
+    # each paper is labeled 1 automatically at the very beginning
+    # then we need to label each paper manually
     header=['paper_id','title','authors','paper_number','label']
     for k in range(len(header)):
         worksheet.write(0,k,header[k])
@@ -330,6 +546,108 @@ def generate_SVM(corpus_matrix,y,indices,spreadsheet_match):
         for j in range(len(spreadsheet_match[0])):
             worksheet.write(i,j,spreadsheet_match[res[i-1]][j])
     workbook.close()
+
+
+
+def randomforests_predict(corpus_matrix,y,indices,spreadsheet_match):
+
+    clf=RandomForestClassifier(n_estimators=300, n_jobs=12, bootstrap=False)
+    train_features=[]
+    for i in range(len(corpus_matrix)):
+        if i in indices:
+            # train on 200 papers
+            train_features.append(corpus_matrix[i])
+
+    # predict on the entire dataset
+    y_pred=clf.fit(train_features,y).predict(corpus_matrix)
+
+
+    workbook=xlsxwriter.Workbook('spreadsheet_randomforests.xlsx')
+    worksheet=workbook.add_worksheet('My sheet')
+
+    # rewrite features and labels to a file, this is different from the initial file
+    # write the header first
+    header=['paper_id','title','authors','paper_number','label']
+    for k in range(len(header)):
+        worksheet.write(0,k,header[k])
+
+    for i in range(1, len(corpus_matrix) + 1):
+        for j in range(len(spreadsheet_match[0])):
+            worksheet.write(i, j, spreadsheet_match[i-1][j])
+            worksheet.write(i, 4, y_pred[i-1])
+    workbook.close()
+
+
+
+def test(spreadsheet_match):
+
+    record=[201,202,203]
+    y_pred=[0,1,0]
+    workbook = xlsxwriter.Workbook('spreadsheet_randomforests.xlsx')
+    worksheet = workbook.add_worksheet('My sheet')
+
+    # rewrite a file, this is different from the initial one
+    header = ['paper_id', 'title', 'authors', 'paper_number', 'label']
+    for k in range(len(header)):
+        worksheet.write(0, k, header[k])
+
+    loc = ('labels.xlsx')
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    sheet.cell_value(0, 0)
+
+    # return labels and number of the paper
+    for i in range(1, sheet.nrows):
+        for j in range(len(spreadsheet_match[0])):
+            worksheet.write(i, j, sheet.cell_value(i, j))
+
+    for p in range(2001, 2001 + len(record)):
+        for q in range(len(spreadsheet_match[0])):
+            worksheet.write(p, q, spreadsheet_match[record[p - 2001]][q])
+            worksheet.write(p, 4, y_pred[p - 2001])
+    workbook.close()
+
+
+
+def test_randomforests(corpus_matrix):
+    y = []
+    indices = []
+    loc = ('spreadsheet_randomforests.xlsx')
+
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    sheet.cell_value(0, 0)
+
+    # return labels and position of the paper
+    for i in range(1, sheet.nrows):
+        y.append(int(sheet.cell_value(i, 4)))
+        indices.append(int(sheet.cell_value(i, 3)))
+
+    # shuffle the entire dataset
+    tmp=list(zip(indices,y))
+    random.shuffle(tmp)
+    indices,y=zip(*tmp)
+
+    # split the data set
+    training_features=[]
+    training_labels=[]
+    testing_features=[]
+    testing_labels=[]
+    # 70% for training
+    for i in range(43000):
+        training_features.append(corpus_matrix[indices[i]])
+        training_labels.append(y[i])
+    # 30% for testing, 62548 papers in total
+    for j in range(43000,len(indices)):
+        testing_features.append(corpus_matrix[indices[j]])
+        testing_labels.append(y[j])
+
+    # use randomforests to make the prediction
+    clf=RandomForestClassifier(n_estimators=300,n_jobs=12,bootstrap=False)
+    clf.fit(training_features,training_labels)
+    y_pred=clf.predict(testing_features)
+    print("Accuracy:", accuracy_score(testing_labels, y_pred))
+
 
 
 
@@ -345,10 +663,15 @@ word_cloud_advanced(keywords_all_text)
 
 
 """
+this is for test purpose
+"""
+# test(spreadsheet_match)
+
+
+"""
 dimension reduction
 """
-
-generate_histogram(corona_body_all_text)
+# generate_histogram(corona_body_all_text)
 
 body_all_text = [preprocess_stem_clean(x) for x in corona_body_all_text]
 corpus_matrix,word_feature_list=generate_tfidf(body_all_text)
@@ -363,10 +686,22 @@ spreadsheet visualization
 
 
 """
-SVM iterations
+SVM iterations to generate the pre-trained model
+this also includes 3 hyperparameter tune models 
 """
 y,indices=generate_labels()
-generate_SVM(corpus_matrix,y,indices,spreadsheet_match)
+# generate_SVM(corpus_matrix,y,indices,spreadsheet_match)
+# grid_search(corpus_matrix,y,indices,spreadsheet_match)
+k_fold_svm(corpus_matrix,y,indices,spreadsheet_match)
+
+
+
+"""
+randomforests to test the pre-trained model
+"""
+# randomforests_predict(corpus_matrix,y,indices,spreadsheet_match)
+# test_randomforests(corpus_matrix)
+
 
 
 """
